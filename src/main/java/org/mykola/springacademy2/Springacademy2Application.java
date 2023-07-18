e3c4ddb0c5fcc7a5cc478959a2dd3ffb73441c7b
Grigorich <oskinnick@gmail.com>
2023-07-18 21:05:21 +0300

    Initial commit

---
+++ src/main/java/org/mykola/springacademy2/Springacademy2Application.java
@@ -0,0 +1,13 @@
+package org.mykola.springacademy2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springacademy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springacademy2Application.class, args);
+	}
+
+}
