7b1911b7e7fdafca72b5e443881d883a0debf432
Grigorich <oskinnick@gmail.com>
2023-07-24 16:37:54 +0300

    POST method construct Tests resolved

--- src/test/java/org/mykola/springacademy2/Springacademy2ApplicationTests.java
+++ src/test/java/org/mykola/springacademy2/Springacademy2ApplicationTests.java
@@ -34,30 +34,30 @@ class CashCardApplicationTests {
 		assertThat(getResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
 
-//	@Test
-//	void shouldNotReturnACashCardWithAnUnknownId() {
-//		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
-//
-//		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
-//		assertThat(response.getBody()).isBlank();
-//	}
+	@Test
+	void shouldNotReturnACashCardWithAnUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/1000", String.class);
 
-//	@Test
-//	void shouldReturnACashCardWhenDataIsSaved() {
-//		ResponseEntity<String> response = restTemplate
-//				.getForEntity("/cashcards/99", String.class);
-//
-//		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
-//
-//		DocumentContext documentContext = JsonPath.parse(response.getBody());
-//		Number id = documentContext.read("$.id");
-//		assertThat(id).isNotNull();
-//		assertThat(id).isEqualTo(99);
-//
-//		Double amount=documentContext.read("$.amount");
-//		assertThat(amount).isNotEqualTo(0.0);
-//		assertThat(amount).isEqualTo(123.45);
-//	}
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
+
+	@Test
+	void shouldReturnACashCardWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate
+				.getForEntity("/cashcards/99", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+		Number id = documentContext.read("$.id");
+		assertThat(id).isNotNull();
+		assertThat(id).isEqualTo(99);
+
+		Double amount=documentContext.read("$.amount");
+		assertThat(amount).isNotEqualTo(0.0);
+		assertThat(amount).isEqualTo(123.45);
+	}
 
 	@Test
 	void contextLoads() {
